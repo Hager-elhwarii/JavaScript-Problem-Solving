5a4a07d23fffe1886d8af30cc0eac092aaaf4e83
hager-dot <hagerelhwarii6633@gmail.com>
2024-04-23 21:13:35 +0200

    :sparkles: Sum of an array of numbers

--- array-sum.js
+++ array-sum.js
@@ -1,12 +1,12 @@
 function getArraySum(numbers) {
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        const element = numbers[i];
-        sum = sum + element;
-    }
-    return sum;
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    sum = sum + element;
+  }
+  return sum;
 }
-const numbers = [1, 2, 3, 6, 5, 5, 4, 2 ]
+const numbers = [1, 2, 7, 6, 20, 5, 4, 33];
 const result = getArraySum(numbers);
 console.log(result);
 //expected output: 28
