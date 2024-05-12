f5b9b0c9abe3f3b776b5b446d16a8d8ed79dcd97
hager-dot <hagerelhwarii6633@gmail.com>
2024-05-12 23:49:44 +0300

    :sparkles: Find the average inside an array

--- average.js
+++ average.js
@@ -1,8 +1,10 @@
 //How to find the average inside an array.
 const numbersArray = [2, 5, 40, 33, 76, 80, 12, 96, 20];
 
+// Using reduce.
 const sum = numbersArray.reduce((accumlator,currentValue)=> {
     return (accumlator + currentValue) 
 },0)
 const averageNumber = Math.round(sum / numbersArray.length)
 console.log({averageNumber});
+//expected output: 40
