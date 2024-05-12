d192f00d9b62c7b43641df087c10069872d6b4af
hager-dot <hagerelhwarii6633@gmail.com>
2024-05-12 23:37:45 +0300

    :sparkles: Add sort method

---
+++ sort.js
@@ -0,0 +1,6 @@
+const numbers = ["G","P","T","A","L","S","Q","Y"];
+
+const sortedArray = numbers.sort()
+
+console.log({sortedArray});
+//expected output: ['A', 'G', 'L','P', 'Q', 'S', 'T', 'Y' ]
--- sum.js
+++ sum.js
@@ -1,4 +1,6 @@
 // Find sum of the numbers inside an array.
+const numbers = [1, 2, 7, 6, 20, 5, 4, 33];
+
 function getArraySum(numbers) {
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
@@ -7,7 +9,6 @@ function getArraySum(numbers) {
   }
   return sum;
 }
-const numbers = [1, 2, 7, 6, 20, 5, 4, 33];
 const result = getArraySum(numbers);
 console.log(result);
 //expected output: 28
