57dfa7db0534f7ecfb856269d6975fae13944cc6
hager-dot <hagerelhwarii6633@gmail.com>
2024-05-18 22:56:30 +0300

    Sort an array of numbers

--- sort.js
+++ sort.js
@@ -1,6 +1,10 @@
-const numbers = ["G","P","T","A","L","S","Q","Y"];
+const numbers = [20,8,17,3,12,9];
 
-const sortedArray = numbers.sort()
+const sortedArray = numbers.sort(function compare(a,b) {
+    return a - b;
+})
 
 console.log({sortedArray});
-//expected output: ['A', 'G', 'L','P', 'Q', 'S', 'T', 'Y' ]
+//expected output: [ 3, 8, 9, 12, 17, 20 ]
+
+
