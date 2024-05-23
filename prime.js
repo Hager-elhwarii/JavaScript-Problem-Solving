2170682f88d97dae1b1fd9da8dfc7e97bc760d07
hager-dot <hagerelhwarii6633@gmail.com>
2024-05-23 23:06:32 +0300

    :sparkles: Find prime number

--- prime.js
+++ prime.js
@@ -1,10 +1,10 @@
 function primeNumber (n){
     for(let i = 2; i < n; i++){
         if(n % i == 0 ){
-            return 'this is not prime number'
+            return 'This is not a prime number'
         }
         else{
-           return 'its prime number'
+           return 'It\'s prime number'
         }
     }
 }
